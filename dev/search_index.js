08d94192641e671d6054ed6a97d7bfe74963b960
Documenter.jl <documenter@juliadocs.github.io>
2025-02-27 15:58:40 +0000

    build based on 1a4f3e4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = enso_project","category":"page"},{"location":"#enso_project","page":"Home","title":"enso_project","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for enso_project.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [enso_project]","category":"page"}]
+}
