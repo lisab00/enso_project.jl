7831121b7007df670bfba033398c674f6bec6cf4
pinkeA <93255488+pinkeA@users.noreply.github.com>
2025-03-19 15:48:23 +0100

    add tests for nde

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,8 @@
 using enso_project
-using Test, ReservoirComputing
+using Test, ReservoirComputing, OrdinaryDiffEq, SciMLSensitivity, Flux
 
 @testset "enso_project.jl" begin
     # Write your tests here.
     include("test_esn.jl")
+    include("test_nde.jl")
 end
